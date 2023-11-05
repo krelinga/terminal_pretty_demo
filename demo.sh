de95362e64135e5970e6bb5d54c6f05b5c5572d5
Andy Kreling <krelinga@gmail.com>
2023-11-04 22:08:54 -0500

    Introduce a variable for background red.

--- demo.sh
+++ demo.sh
@@ -2,6 +2,7 @@
 
 # Some common useful codes
 c_reset='\u001b[0m'
+c_bg_red='\u001b[41m'
 
 echo "got here!"
-printf "\u001b[37;1m\u001b[41mtest${c_reset}\u001b[31m\uE0B0\n"
+printf "\u001b[37;1m${c_bg_red}test${c_reset}\u001b[31m\uE0B0\n"
