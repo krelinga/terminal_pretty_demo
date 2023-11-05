cc66d5df88840ab60ea085d75122e589e3ec038b
Andy Kreling <krelinga@gmail.com>
2023-11-04 22:37:35 -0500

    demonstrate a helper bash function forcolor manipulation.

--- demo.sh
+++ demo.sh
@@ -12,4 +12,28 @@ c_fg_red='\u001b[31m'
 # Bright foreground colors
 c_fg_bwhite='\u001b[37;1m'
 
-printf "${c_fg_bwhite}${c_bg_red}test${c_reset}${c_fg_red}\uE0B0\n"
+readonly w_red="w_red"
+readonly w_bwhite="w_bwhite"
+
+function Write() {
+    readonly in_fg_color="$1"
+    readonly in_bg_color="$2"
+    readonly in_text="$3"
+
+    case "${in_fg_color}" in
+        "${w_red}")
+            printf '\u001b[31m' ;;
+        "${w_bwhite}")
+            printf '\u001b[37;1m' ;;
+    esac
+
+    case "${in_bg_color}" in
+        "${w_red}")
+            printf '\u001b[41m' ;;
+    esac
+
+    printf "${in_text}\u001b[0m"
+}
+
+Write w_bwhite w_red "test"
+printf "${c_reset}${c_fg_red}\uE0B0\n"
