627a7cc6d4c3c3aa0095b95509b5234ef66721f2
Andy Kreling <krelinga@gmail.com>
2023-11-04 22:13:52 -0500

    factor out the code for foreground red.

--- demo.sh
+++ demo.sh
@@ -6,7 +6,10 @@ c_reset='\u001b[0m'
 # Background colors
 c_bg_red='\u001b[41m'
 
+# Foreground colrs
+c_fg_red='\u001b[31m'
+
 # Bright foreground colors
 c_fg_bwhite='\u001b[37;1m'
 
-printf "${c_fg_bwhite}${c_bg_red}test${c_reset}\u001b[31m\uE0B0\n"
+printf "${c_fg_bwhite}${c_bg_red}test${c_reset}${c_fg_red}\uE0B0\n"
