2d4f4610f903ee1b06d83c5f18b0a6f04b44e37f
Andy Kreling <krelinga@gmail.com>
2023-11-04 22:07:10 -0500

    Demonstrate that shell variables work with color codes.

--- demo.sh
+++ demo.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/bash
 
+# Some common useful codes
+c_reset='\u001b[0m'
+
 echo "got here!"
-printf '\u001b[37;1m\u001b[41mtest\u001b[0m\u001b[31m\uE0B0\n'
+printf "\u001b[37;1m\u001b[41mtest${c_reset}\u001b[31m\uE0B0\n"
